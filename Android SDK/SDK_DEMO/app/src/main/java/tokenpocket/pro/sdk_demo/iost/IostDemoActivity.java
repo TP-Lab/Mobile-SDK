195f409df71ed6fff294dc48b1cce81adbc43d7e
NullPointerDuke <ysoftdev@163.com>
2022-04-02 22:55:45 +0800

    Update IostDemoActivity.java

--- Android SDK/SDK_DEMO/app/src/main/java/tokenpocket/pro/sdk_demo/iost/IostDemoActivity.java
+++ Android SDK/SDK_DEMO/app/src/main/java/tokenpocket/pro/sdk_demo/iost/IostDemoActivity.java
@@ -68,12 +68,15 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
         authorize.setVersion("1.0");
         authorize.setDappName("Test demo");
         authorize.setDappIcon("https://eosknights.io/img/icon.png");
+        //开发者自己定义的业务id，用来标识本次操作,授权操作一定要设置这个参数
         authorize.setActionId("web-db4c5466-1a03-438c-90c9-2172e8becea5");
-        authorize.setMemo("demo");
         authorize.setAction("login");
+        //开发者服务端提供的接受调用登录结果的接口，如果设置该参数，钱包操作完成后，会将结果通过post application json方式将结果回调给callbackurl
+        authorize.setCallbackUrl("http://115.205.0.178:9011/taaBizApi/taaInitData");
         TPManager.getInstance().authorize(this, authorize, new TPListener() {
             @Override
             public void onSuccess(String s) {
+                //登录成功后，会返回登录的钱包地址，签名信息，开发者可以利用这两个参数验证有效性
                 Toast.makeText(IostDemoActivity.this, s, Toast.LENGTH_LONG).show();
             }
 
@@ -97,13 +100,17 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
         signature.setVersion("1.0");
         signature.setDappName("Test demo");
         signature.setDappIcon("https://eosknights.io/img/icon.png");
+        //开发者自己定义的业务id，用来标识本次操作
         signature.setActionId("web-db4c5466-1a03-438c-90c9-2172e8becea5");
-        signature.setMemo("demo");
         signature.setAction("sign");
+        //待签名的字符串
         signature.setMessage("message to sign");
+        //开发者服务端提供的接受调用登录结果的接口，如果设置该参数，钱包操作完成后，会将结果通过post application json方式将结果回调给callbackurl
+        signature.setCallbackUrl("http://115.205.0.178:9011/taaBizApi/taaInitData");
         TPManager.getInstance().signature(this, signature, new TPListener() {
             @Override
             public void onSuccess(String s) {
+                //签名完成后，会返回签名的钱包信息和签名信息
                 Toast.makeText(IostDemoActivity.this, s, Toast.LENGTH_LONG).show();
             }
 
@@ -126,19 +133,25 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
         transfer.setVersion("1.0");
         transfer.setDappName("Test demo");
         transfer.setDappIcon("https://eosknights.io/img/icon.png");
+        //开发者自己定义的业务id，用来标识本次操作
         transfer.setActionId("web-db4c5466-1a03-438c-90c9-2172e8becea5");
         transfer.setMemo("demo");
         transfer.setAction("transfer");
         transfer.setFrom("mingyi1");
         transfer.setTo("_bbbb");
+        //代币精度，一定要准确
         transfer.setPrecision(4);
         transfer.setContract("token.iost");
         transfer.setAmount(0.001);
         transfer.setSymbol("IOST");
+        //仅供UI展示，不上链
         transfer.setDesc("");
+        //开发者服务端提供的接受调用登录结果的接口，如果设置该参数，钱包操作完成后，会将结果通过post application json方式将结果回调给callbackurl
+        transfer.setCallbackUrl("http://115.205.0.178:9011/taaBizApi/taaInitData");
         TPManager.getInstance().transfer(this, transfer, new TPListener() {
             @Override
             public void onSuccess(String s) {
+                //转账成功后，会返回相应的交易hash，注意，钱包只是把交易push出去，并不能保证最后交易结果，开发者需要根据hash自行确定链上结果
                 Toast.makeText(IostDemoActivity.this, s, Toast.LENGTH_LONG).show();
             }
 
@@ -154,6 +167,9 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
         });
     }
 
+    /**
+     * 通用合约操作，下面以投票为实例展示
+     */
     private void pushTx() {
         Transaction transaction = new Transaction();
         transaction.setBlockchain("IOST");
@@ -161,14 +177,20 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
         transaction.setVersion("1.0");
         transaction.setDappName("Test demo");
         transaction.setDappIcon("https://eosknights.io/img/icon.png");
+        //开发者自己定义的业务id，用来标识本次操作
         transaction.setActionId("web-db4c5466-1a03-438c-90c9-2172e8becea5");
         transaction.setAction("pushTransaction");
+        //iost中，pushTx操作实际上是对构造好的交易对象进行签名，要求的参数和iost插件钱包签名数据格式一样，
+        // 具体生成方式，可以参照iost开发文档:https://developers.iost.io/docs/zh-CN/1-getting-started/Overview.html
+        //开发者服务端提供的接受调用登录结果的接口，如果设置该参数，钱包操作完成后，会将结果通过post application json方式将结果回调给callbackurl
+        transaction.setCallbackUrl("http://115.205.0.178:9011/taaBizApi/taaInitData");
         transaction.setPayload("{\n" +
                 "\t\t\"tx\": {\n" +
                 "\t\t\t\"gasRatio\": 1,\n" +
                 "\t\t\t\"gasLimit\": 2000000,\n" +
                 "\t\t\t\"actions\": [{\n" +
-                "\t\t\t\t\"contract\": \"ContractF3tLtxdXwYmKsDiUtTmaQztwJQLPVf9VyWDqufMZHP5p\",\n" +
+                "\t\t\t\t\"contract\": \"ContractF3tLtxdXwYmKsDiUtTmaQztwJQLPVf9VyWDqufMZHP5p\"," +
+                "\n" +
                 "\t\t\t\t\"actionName\": \"vote\",\n" +
                 "\t\t\t\t\"data\": \"[\\\"mingyi\\\",\\\"tokenpocket\\\",\\\"0.01\\\"]\"\n" +
                 "\t\t\t}],\n" +
@@ -189,11 +211,13 @@ public class IostDemoActivity extends Activity implements View.OnClickListener {
                 "\t\t\"domain\": \"tool.l6iebi.com\",\n" +
                 "\t\t\"account\": \"mingyi\",\n" +
                 "\t\t\"network\": \"MAINNET\",\n" +
-                "\t\t\"txABI\": [\"ContractF3tLtxdXwYmKsDiUtTmaQztwJQLPVf9VyWDqufMZHP5p\", \"vote\", [\"mingyi\", \"tokenpocket\", \"0.01\"]]\n" +
+                "\t\t\"txABI\": [\"ContractF3tLtxdXwYmKsDiUtTmaQztwJQLPVf9VyWDqufMZHP5p\", " +
+                "\"vote\", [\"mingyi\", \"tokenpocket\", \"0.01\"]]\n" +
                 "\t}");
         TPManager.getInstance().pushTransaction(this, transaction, new TPListener() {
             @Override
             public void onSuccess(String s) {
+                //转账成功后，会返回相应的交易hash，注意，钱包只是把交易push出去，并不能保证最后交易结果，开发者需要根据hash自行确定链上结果
                 Toast.makeText(IostDemoActivity.this, s, Toast.LENGTH_LONG).show();
             }
 
